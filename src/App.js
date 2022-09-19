587c5e142f21c06771f3754e56b8fa51098066a3
Corey Lott <Lott-c@hotmail.com>
2022-09-19 13:22:49 -0500

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Container } from '@mui/system';
 
 function App() {
   return (
-    <>
-    <Header />
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-    </>
-    
+    <Router>
+      <Header />
+        <div className="App">
+          <Container>
+            <Route exact path='/' component={Trending} /> 
+              <Route path='/movies' component={Movies} /> 
+              <Route path='/shows' component={Shows} /> 
+              <Route path='/search' component={Search} /> 
+          </Container>
+        </div>
+      <SimpleBottomNavigation />
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import SearchIcon from '@mui/icons-material/Search';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import MovieIcon from '@mui/icons-material/Movie';
+import TvIcon from '@mui/icons-material/Tv';
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
 
-const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <Box sx={{ width: '100%', position: 'fixed', bottom: 0, zIndex: 100, }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        
+      >
+        <BottomNavigationAction label="Trending" icon={<TrendingUpIcon />} />
+        <BottomNavigationAction label="Movies" icon={<MovieIcon />} />
+        <BottomNavigationAction label="Tv Shows" icon={<TvIcon />} />
+        <BottomNavigationAction label="Search" icon={<SearchIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
 }
-
-export default Navbar
