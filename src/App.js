37b24aee9e98c78cea56403da68ebd97d87da4dd
Corey Lott <Lott-c@hotmail.com>
2022-09-19 10:51:54 -0500

    added components header is looking good

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
+    <>
+    <Header />
     <div className="App">
       <h1>Hello</h1>
     </div>
+    </>
+    
   );
 }
 
